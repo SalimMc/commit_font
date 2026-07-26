9cbcaa4d451d8fc5b0259f8ed6ff92489e1936cf
SalimMcPicsou <185490663+SalimMc@users.noreply.github.com>
2026-07-26 14:13:58 +0200

    feat(letter_c): implement letter c font

---
+++ src/symbols/letter_c.sh
@@ -0,0 +1,15 @@
+START_DATE="2019-01-06" #The beginning date of your commits
+
+#-----------------------------------------------------------------------
+for i in 3 4 9 12 16 19
+do
+    DATE=$(date -d "$START_DATE +$i days 12:00:00" +"%Y-%m-%dT%H:%M:%S")
+
+    echo "$DATE" >> modifier.txt
+    git add modifier.txt
+
+    GIT_AUTHOR_DATE="$DATE" \
+    GIT_COMMITTER_DATE="$DATE" \
+    git commit -m "Letter c - commit pixel: $i"
+done
+#-----------------------------------------------------------------------
