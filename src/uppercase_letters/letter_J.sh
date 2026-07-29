a480564711eb3d70fa91b6c7036be036ba4ca284
SalimMcPicsou <185490663+SalimMc@users.noreply.github.com>
2026-07-29 18:37:14 +0200

    feat(letter_J): implement letter J font

---
+++ src/uppercase_letters/letter_J.sh
@@ -0,0 +1,15 @@
+START_DATE="2019-01-06" #The beginning date of your commits
+
+#-----------------------------------------------------------------------
+for i in 4 12 19 22 23 24 25
+do
+    DATE=$(date -d "$START_DATE +$i days 12:00:00" +"%Y-%m-%dT%H:%M:%S")
+
+    echo "$DATE" >> modifier.txt
+    git add modifier.txt
+
+    GIT_AUTHOR_DATE="$DATE" \
+    GIT_COMMITTER_DATE="$DATE" \
+    git commit -m "Letter J - commit pixel: $i"
+done
+#-----------------------------------------------------------------------
