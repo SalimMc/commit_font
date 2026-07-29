24f6ccdb6ec8c1a68a2fdb26851210806f715066
SalimMcPicsou <185490663+SalimMc@users.noreply.github.com>
2026-07-29 18:31:06 +0200

    feat(letter_F): implement letter F font

---
+++ src/uppercase_letters/letter_F.sh
@@ -0,0 +1,15 @@
+START_DATE="2019-01-06" #The beginning date of your commits
+
+#-----------------------------------------------------------------------
+for i in 1 2 3 4 5 8 10 15 17 22
+do
+    DATE=$(date -d "$START_DATE +$i days 12:00:00" +"%Y-%m-%dT%H:%M:%S")
+
+    echo "$DATE" >> modifier.txt
+    git add modifier.txt
+
+    GIT_AUTHOR_DATE="$DATE" \
+    GIT_COMMITTER_DATE="$DATE" \
+    git commit -m "Letter F - commit pixel: $i"
+done
+#-----------------------------------------------------------------------
