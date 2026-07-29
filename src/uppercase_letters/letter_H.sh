030e3b9069c8e1d0e065cd0d9dd75546458c4a4c
SalimMcPicsou <185490663+SalimMc@users.noreply.github.com>
2026-07-29 18:35:52 +0200

    feat(letter_H): implement letter H font

---
+++ src/uppercase_letters/letter_H.sh
@@ -0,0 +1,15 @@
+START_DATE="2019-01-06" #The beginning date of your commits
+
+#-----------------------------------------------------------------------
+for i in 1 2 3 4 5 10 17 22 23 24 25 26
+do
+    DATE=$(date -d "$START_DATE +$i days 12:00:00" +"%Y-%m-%dT%H:%M:%S")
+
+    echo "$DATE" >> modifier.txt
+    git add modifier.txt
+
+    GIT_AUTHOR_DATE="$DATE" \
+    GIT_COMMITTER_DATE="$DATE" \
+    git commit -m "Letter H - commit pixel: $i"
+done
+#-----------------------------------------------------------------------
