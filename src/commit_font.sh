eecf637dec16146dc014f2621a1b35233189ee69
SalimMcPicsou <185490663+SalimMc@users.noreply.github.com>
2026-07-30 17:37:55 +0200

    docs(commit_font): doc first loop example

--- src/commit_font.sh
+++ src/commit_font.sh
@@ -1,16 +1,17 @@
 START_DATE="2023-02-26" #The beginning date of the commit for the first letter
 
 #-----------------------------------------------------------------------
-for i in 2 5 8 10 12 15 17 19 22 25
+for i in 2 5 8 10 12 15 17 19 22 25 #The corresponding pixel indexes to be coloured/commited in a for-loop
 do
-    DATE=$(date -d "$START_DATE +$i days 12:00:00" +"%Y-%m-%dT%H:%M:%S")
+    DATE=$(date -d "$START_DATE +$i days 12:00:00" +"%Y-%m-%dT%H:%M:%S") #Computes the new date according to the pixel index
 
-    echo "$DATE" >> modifier.txt
-    git add modifier.txt
+    echo "$DATE" >> modifier.txt #Modify a file to allow to create a new commit with a content
+    git add modifier.txt #Add the file modification to the commit
 
+    #Set the date to the corresponding in the loop
     GIT_AUTHOR_DATE="$DATE" \
     GIT_COMMITTER_DATE="$DATE" \
-    git commit -m "Letter S - commit pixel: $i"
+    git commit -m "Letter S - commit pixel: $i" #Make the commit
 done
 #-----------------------------------------------------------------------
 
